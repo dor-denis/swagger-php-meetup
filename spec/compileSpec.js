8af8a0678980dc16f1b67a652e635746509d69ac
Denys Dorofeiev <denys.dorofeiev@westwing.de>
2017-11-12 14:11:34 +0100

    Finish demo spec

--- spec/compileSpec.js
+++ spec/compileSpec.js
@@ -1,10 +1,11 @@
 var refParser = require('json-schema-ref-parser');
+var YAML = require('json2yaml');
 
 refParser.bundle('index.yaml', function(err, schema) {
   if (err) {
     console.error(err);
   }
   else {
-    console.log(JSON.stringify(schema, null, 2));
+    console.log(YAML.stringify(schema));
   }
 });
